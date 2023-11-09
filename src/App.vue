7d0cf50aaf43f20fd8e273d93127f7b409df958e
MatheusCBarreto <devmattbarreto@protonmail.com>
2023-11-09 14:10:26 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,36 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="App">
+    <div v-for="(poke, index) in pokemons" :key="index">
+      <Pokemons :name="poke.name" :url="poke.url" :num="index + 1"/>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
+import Pokemons from "./components/Pokemons"
 
 export default {
   name: 'App',
+  data() {
+    return {
+      pokemons: []
+    }
+  },
+  created: function() {
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0")
+      .then(res => {
+        this.pokemons = res.data.results;
+      });
+  },
   components: {
-    HelloWorld
+    Pokemons,
   }
 }
 </script>
 
 <style>
-#app {
+#App {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
---
+++ src/components/Pokemons.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>{{ num }} {{ upper(name)}}</h1>
+    <small>{{ url }}</small>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    num: Number,
+    name: String,
+    url: String
+  },
+  methods: {
+    upper: function(value) {
+      let newName = value[0].toUpperCase() + value.slice(1);
+      return newName
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
